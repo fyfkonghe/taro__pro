d0af2660e2be7648542aa78395a3aaae367695cd
fu <153674111@qq.com>
2023-04-24 17:04:58 +0800

    feat: 解决ts报错，编译配置

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -3,7 +3,7 @@ import { defineStore } from 'pinia'
 
 export const useCounterStore = defineStore('counter', {
   state: () => {
-    return { count: 0 }
+    return { count: 30 }
   },
   // could also be defined as
   // state: () => ({ count: 0 })
